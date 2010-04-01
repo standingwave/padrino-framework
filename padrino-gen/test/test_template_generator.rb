fee009dfced07d87229a28290a8a18fb4c3d970a
achiu <mr.arthur.chiu@gmail.com>
2010-04-01 08:25:35 -0700

    cleaned up tests a bit

--- padrino-gen/test/test_template_generator.rb
+++ padrino-gen/test/test_template_generator.rb
@@ -7,11 +7,11 @@ class TestTemplateGenerator < Test::Unit::TestCase
     FakeWeb.allow_net_connect = false
     `rm -rf /tmp/sample_project`
     @template = Padrino::Generators::Template.dup
+    silence_logger { @template.start(['sample_project', 'example_template.rb','-r=/tmp']) }
   end
 
   context 'the template generator' do
     should "allow template generator to generate project scaffold" do
-      assert_nothing_raised { silence_logger { @template.start(['sample_project', 'example_template.rb','-r=/tmp']) } }
       assert_file_exists('/tmp/sample_project')
       assert_file_exists('/tmp/sample_project/app')
       assert_file_exists('/tmp/sample_project/config/boot.rb')
@@ -20,20 +20,17 @@ class TestTemplateGenerator < Test::Unit::TestCase
     end
     
     should "create models" do
-      assert_nothing_raised { silence_logger { @template.start(['sample_project', 'example_template.rb','-r=/tmp/']) } }
       assert_file_exists('/tmp/sample_project/app/models/post.rb')
       assert_match_in_file(/class Post/, '/tmp/sample_project/app/models/post.rb')
     end
     
     should "create controllers" do
-      assert_nothing_raised { silence_logger { @template.start(['sample_project', 'example_template.rb','-r=/tmp/']) } }
       assert_file_exists('/tmp/sample_project/app/controllers/posts.rb')
       assert_match_in_file(/:posts/, '/tmp/sample_project/app/controllers/posts.rb')
       assert_match_in_file(/get :index/,'/tmp/sample_project/app/controllers/posts.rb')
     end
     
     should "create migrations" do
-      assert_nothing_raised { silence_logger { @template.start(['sample_project', 'example_template.rb','-r=/tmp/']) } }
       migration_file_path = "/tmp/sample_project/db/migrate/002_add_email_to_user.rb"
       assert_match_in_file(/class AddEmailToUser/m, migration_file_path)
       assert_match_in_file(/t.string :email/, migration_file_path)
