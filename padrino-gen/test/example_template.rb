8288bd28e06c17b0dbe6f376216ae069e4631a85
achiu <mr.arthur.chiu@gmail.com>
2010-04-01 18:39:15 -0700

    added future syntax for template

--- padrino-gen/test/example_template.rb
+++ padrino-gen/test/example_template.rb
@@ -3,10 +3,24 @@ project :test => :rspec, :orm => :activerecord
 generate :model, "post title:string body:text"
 generate :controller, "posts get:index get:new post:new"
 generate :migration, "AddEmailToUser email:string"
-generate :fake, "AddEmailToUser email:string"
+generate :fake, "foo bar"
 
 require_dependencies 'nokogiri'
 
 inject_into_file "app/models/post.rb","#Hello", :after => "end\n"
 
 initializer :test, "#Hello"
+
+=begin
+
+  project(:test => :rspec, :orm => :activerecord) do
+    generate :model, "post title:string body:text"
+    app("test") do
+      generate :controller, "posts get:index"
+    end
+  end
+  
+  require_dependencies 'nokogiri'
+  initializer :test, "hello"
+
+=end
