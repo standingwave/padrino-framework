edf6c030652de871e8e6ed6a677b1c58c1d67376
achiu <mr.arthur.chiu@gmail.com>
2010-04-03 21:53:09 -0700

    updated less_gen to use initializer

--- padrino-gen/lib/padrino-gen/generators/components/stylesheets/less_gen.rb
+++ padrino-gen/lib/padrino-gen/generators/components/stylesheets/less_gen.rb
@@ -3,27 +3,21 @@ module Padrino
     module Components
       module Stylesheets
         module LessGen
-          LESS_INIT = (<<-LESS).gsub(/^ {10}/, '')
+          LESS_INIT = (<<-LESS).gsub(/^ {6}/, '')
           # Enables support for Less template reloading for rack.
           # Store Less files by default within 'app/stylesheets/'
           # See http://github.com/kelredd/rack-less for more details.
-
-          module LessInitializer
-            def self.registered(app)
-              require 'rack/less'
-              # optional - use as necessary
-              Rack::Less.configure do |config|
-                config.compress = true
-                # other configs ...
-              end
-              app.use Rack::Less,
-              :root      => app.root,
-              :source    => 'stylesheets/',
-              :public    => 'public/',
-              :hosted_at => '/stylesheets'
-            end
+          require 'rack/less'
+          # optional - use as necessary
+          Rack::Less.configure do |config|
+            config.compress = true
+            # other configs ...
           end
-
+          app.use Rack::Less,
+          :root      => app.root,
+          :source    => 'stylesheets/',
+          :public    => 'public/',
+          :hosted_at => '/stylesheets'
           LESS
 
           LESS_REGISTER = (<<-LESSR).gsub(/^ {10}/, '')
@@ -32,8 +26,7 @@ module Padrino
 
           def setup_stylesheet
             require_dependencies 'less', 'rack-less'
-            create_file destination_root('/lib/less_init.rb'), LESS_INIT
-            inject_into_file destination_root('/app/app.rb'), LESS_REGISTER, :after => "configure do\n"
+            initializer :less, LESS_INIT
             empty_directory destination_root('/app/stylesheets')
           end
         end # LessGen
