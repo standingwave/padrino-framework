81f37520fb867a2e28bf833fa012f450c1c0eb51
Nathan Esquenazi <nesquena@gmail.com>
2010-05-07 03:36:17 -0700

    [padrino-mailer] Cleaning up the message extension class

--- padrino-mailer/lib/padrino-mailer/ext.rb
+++ padrino-mailer/lib/padrino-mailer/ext.rb
@@ -3,84 +3,69 @@ module Mail
     include Sinatra::Templates
     include Padrino::Rendering if defined?(Padrino::Rendering)
 
-    # Shortcut
+    # Shortcut for delivery_method
     alias :via :delivery_method
 
-    def initialize(*args, &block)
-      @template_cache        = Tilt::Cache.new
-      @body                  = nil
-      @text_part             = nil
-      @html_part             = nil
-      @errors                = nil
-      @header                = nil
-      @charset               = 'UTF-8'
-      @defaulted_charset     = true
-      @perform_deliveries    = true
-      @raise_delivery_errors = true
-      @delivery_handler      = nil
-      @delivery_method       = Mail.delivery_method.dup
-      @transport_encoding    = Mail::Encodings.get_encoding('7bit')
-
-      # Set a default view for this class
-      settings.views = File.expand_path("./mailers")
-      settings.reload_templates = true
-
+    def initialize_with_app(*args, &block)
+      @template_cache = Tilt::Cache.new
       # Check if we have an app
       if args[0].respond_to?(:views) && args[0].respond_to?(:reload_templates?)
         app                       = args.shift
         settings.views            = File.join(app.views, 'mailers')
         settings.reload_templates = app.reload_templates?
-      end
-
-      if args.flatten.first.respond_to?(:each_pair)
-        init_with_hash(args.flatten.first)
       else
-        init_with_string(args.flatten[0].to_s.strip)
+        # Set a default view for this class
+        settings.views = File.expand_path("./mailers")
+        settings.reload_templates = true
       end
-
-      if block_given?
-        instance_eval(&block)
-      end
-
-      self
+      initialize_without_app(*args, &block)
     end
+    alias_method_chain :initialize, :app
 
-    # Sinatra/Padrino compatibility
+    # Sinatra and Padrino compatibility
     def settings
       self.class
     end
     alias :options :settings
 
+    # Sets the message defined template path to the given view path
     def views(value)
       self.class.views = value
     end
 
+    # Returns the templates for this message
     def self.templates
       @_templates ||= {}
     end
 
+    # Sets the message defined template path to the given view path
     def self.views=(value)
       @_views = value
     end
 
+    # Returns the template view path defined for this message
     def self.views
       @_views
     end
 
+    # Modify whether templates should be reloaded (for development)
     def self.reload_templates=(value)
       @_reload_templates = value
     end
 
+    # Returns true if the templates will be reloaded; false otherwise.
     def self.reload_templates?
       @_reload_templates
     end
 
     private
-      def render(engine, data=nil, options={}, locals={}, &block)
-        # Reload templates
-        @template_cache.clear if settings.reload_templates?
-        # Pass arguments to Sinatra/Padrino render method
-        super(engine, data, options, locals, &block)
-      end
+    
+    # Defines the render for the mailer utilizing the padrino 'rendering' module
+    def render(engine, data=nil, options={}, locals={}, &block)
+      # Reload templates
+      @template_cache.clear if settings.reload_templates?
+      # Pass arguments to Sinatra/Padrino render method
+      super(engine, data, options, locals, &block)
+    end
   end
 end
